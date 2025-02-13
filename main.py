ce4d98b6580250f307af708323f4a585d5eae3ef
corentin <corentin.warzee@student.uclouvain.be>
2025-02-13 17:33:38 +0100

    x

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 
-x=0
-while True:
-    print("Hello World")
-    time.sleep(1)
-    x+=1
-    print(x)
+def main():
+    x=0
+    while True:
+        print("Hello World")
+        time.sleep(1)
+        x+=1
+        print(x)
